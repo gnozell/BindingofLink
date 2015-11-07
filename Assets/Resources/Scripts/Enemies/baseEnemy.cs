cf91435011f8c7b0eba280b6ec82ac13fa100b1f
gnozell <gnozell@yahoo.com>
2015-11-07 13:19:33 -0500

    Planning out Enemies fold structure
    Planning out how the folder structure with enemies like octorocks

---
+++ Assets/Resources/Scripts/Enemies/baseEnemy.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class baseEnemy: MonoBehaviour {
+
+    public GameObject player;
+
+    private bool invulnerable;
+    private bool poisoned;
+
+    private float health;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D other){
+
+    }
+
+    void OnCollisionStay2D(Collision2D other){
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
